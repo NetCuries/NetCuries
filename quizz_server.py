09996f33d17cf2fb61fa8166d75670a39653613d
Ines Belhaj <ines.bel-hadj@medtech.tn>
2023-11-09 17:49:19 +0100

    Modified the file quizz_server to use thread

--- quizz_server.py
+++ quizz_server.py
@@ -3,7 +3,7 @@ import time
 import random
 import threading
 
-users = {}
+
 questions = [
     {'question': 'What is the capital of France?',
      'options': ['A. London', 'B. Berlin', 'C. Paris', 'D. Madrid'],
@@ -15,52 +15,64 @@ questions = [
      'options': ['A. London', 'B. Berlin', 'C. Paris', 'D. Madrid'],
      'correct_answer': 'A'},
 ]
+
+clients = []
 scores = {}
-
-def handle_client(client_socket, username):
-    try:
-        while questions:
-            current_question = random.choice(questions)
-            question_text = current_question['question']
-            options = "\n".join(current_question['options'])
-            client_socket.send(f'Question: {question_text}\n{options}\n'.encode())
-
-            client_socket.settimeout(10)  # Set a timeout of 5 seconds for client's response
-            try:
-                client_answer = client_socket.recv(1024).decode().strip().upper()
-                correct_answer = current_question['correct_answer']
-                if correct_answer == client_answer:
-                    scores[username] = scores.get(username, 0) + 1
-                    client_socket.send('Correct!\n'.encode())
-                else:
-                    client_socket.send(f'Wrong Answer! The correct answer is: {correct_answer}\n'.encode())
-                if not client_answer:
-                    raise socket.timeout
-            except socket.timeout:
-                client_socket.send("Time's up! Better luck next time.\n".encode())
-                continue
-
-            
-
-            client_socket.send(f'Score: {scores.get(username, 0)}\n'.encode())
-            questions.remove(current_question)
-    except Exception as e:
-        print(f'Client {username} disconnected: {e}')
-    finally:
-        client_socket.close()
-
 host = ''
 port = 12222
+time_limit = 30
+
+
+            
+def handle_client(client):
+    username = client.recv(1024).decode()
+    client.send("Welcome to the quiz game, {}!".format(username).encode())
+    
+    #Maximum 3 clients need to connect
+    #while len(clients) < 3:
+           # pass
+        
+    #choose the question and its related answer
+    current_question = random.choice(questions)
+    print("cur",current_question)
+    question_text = current_question['question']
+    options = "\n".join(current_question['options'])
+    correct_answer = current_question['correct_answer']
+    
+    #Display the questions to all clients
+    for client in clients:
+        client.send(f'Question: {question_text}\n{options}\n'.encode())
+        
+    # Start a timer for the current question
+    timer = threading.Timer(time_limit, correct_answer)
+    timer.start()
+    
+    while True:
+        #check if the client answered
+        answer = client.recv(1024).decode()
+        if answer == correct_answer:
+            timer.cancel()
+            scores[username] = scores.get(username, 0) + 1
+            
+            #Display to all clients the correct answer
+            for client in clients:
+                client.send(correct_answer.encode())
+            break
+        
+        #close the client connection
+        client.close()
+        
+
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP
 sock.bind((host, port))
 sock.listen(3)
 print(f"TCP server is listening on {host}:{port}")
 
+
+
 while True:
     client, address = sock.accept()
-    username = client.recv(1024).decode()
-    users[username] = client
-
-    print(f'Accepted connection from {address[0]}:{address[1]} as {username}')
-    client_handler = threading.Thread(target=handle_client, args=(client, username))
-    client_handler.start()
+    clients.append(client)
+    thread = threading.Thread(target=handle_client, args=(client,))
+    thread.start()
